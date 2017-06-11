4fdf4e5fb590a7414253aef3279103f5c9ef381e
Michael-Bryant Choa <michael-bryant.choa@hautelook.com>
2017-06-10 22:58:19 -0700

    Setup CSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export default class App extends Component {
+import HelloWorld from './HelloWorld';
+
+class App extends Component {
+    static childContextTypes = {
+        insertCss: PropTypes.func.isRequired
+    };
+
+    getChildContext() {
+        return {
+            insertCss: (...styles) => {
+                const removeCssArr = styles.map(x => x._insertCss());
+                return () => { removeCssArr.forEach(removeCss => removeCss()); }
+            }
+        };
+    }
+
     render () {
         return (
-            <div>Hello from the React Ecosystem!</div>
+            <div>
+                <HelloWorld />
+            </div>
         );
     }
 }
+
+export default App;
--- tools/webpack.common.js
+++ tools/webpack.common.js
@@ -1,7 +1,30 @@
+import path from 'path';
+
 export default {
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'isomorphic-style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: true
+                        }
+                    },
+                    { loader: 'postcss-loader' }
+                ]
+            }
         ]
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.css']
     }
 }
